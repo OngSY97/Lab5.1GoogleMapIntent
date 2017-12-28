4236e76c7bba62ecb5a6dd644cdc623efadda423
OngSY97 <sheakyer97@hotmail.com>
2017-12-28 14:42:41 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/lab51googlemapintent/MainActivity.java
@@ -0,0 +1,78 @@
+package my.edu.tarc.lab51googlemapintent;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editTextLon, editTextLat;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextLon = (EditText)findViewById(R.id.editTextLon);
+        editTextLat = (EditText)findViewById(R.id.editTextLat);
+
+        Button ButtonShowMap = (Button)findViewById(R.id.buttonShowMap);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        ButtonShowMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Double lat,lon;
+                String query = "cinema";
+                lat = Double.parseDouble(editTextLat.getText().toString());
+                lon = Double.parseDouble(editTextLat.getText().toString());
+                //TODO passing lat and lon values to an Intent object
+                Uri gmmIntentUri = Uri.parse("geo:"+ lat+","+lon+"?z=10").buildUpon()
+                        .appendQueryParameter("q", "1st & Pike, Seattle").build();
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
